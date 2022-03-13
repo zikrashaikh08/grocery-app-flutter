7130d78abe42553ef482a22ffe43a93f8edad798
zikrashaikh08 <zikrashaikh7977@gmail.com>
2022-03-13 14:10:33 +0530

    new commit

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_app/screens/welcome_screen.dart';
@@ -10,24 +12,66 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
+    var preferredSize3 = PreferredSize(
+          preferredSize: const Size.fromHeight(56),
+          child: Padding(
+            padding: const EdgeInserts.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText:'Search',
+                prefixIcon: Icon(Icons.search,color: Colors.grey,),
+                border: OutlineInputBorder(
+                  borderRadius:BorderRadius.circular(5),
+                  borderSide:BorderSide.none,
+                ),
+                contentPadding: EdgeInserts.zero,
+	              filled: true,
+                fillColor: Colors.white,
+	           ),
+           ),
+	       ),
+        );
+    var preferredSize2 = preferredSize3;
     return Scaffold(
-      body: Center(
-          child: Column(
+      appBar: AppBar(
+        elevation: 0.0,
+        leading: Container(),
+        title: FlatButton(
+          onPressed: () {   
+          },
+          child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
+              Text('Delivery Address',style: TextStyle(color: Colors.white),),
+              Icon(Icons.edit_outlined,color: Colors.white,),
+            ],
+
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: IconButton(icon: Icon(Icons.account_circle_outlined),onPressed: (){},),
+          ),
+        ],
+        centerTitle: true,
+        bottom: preferredSize2,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
             RaisedButton(
               onPressed: () {
                 auth.error = '';
                 FirebaseAuth.instance.signOut().then((value) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
+                  Navigator.push(context,MaterialPageRoute(
                       builder: (context) => WelcomeScreen(),
-                    ));
-                  });
-                },
-                child: Text('Sign Out'),
-              ),
+                  ));
+                });
+              },
+              child: Text('Sign Out'),
+            ),
               RaisedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, WelcomeScreen.id);
@@ -39,4 +83,8 @@ class HomeScreen extends StatelessWidget {
         ),
     );
   }
+}
+
+class EdgeInserts {
+  static var zero;
 }
